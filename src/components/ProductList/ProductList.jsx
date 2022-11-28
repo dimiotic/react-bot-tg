483228bb76731f17164b6ac2cc3fd24c5eded551
John Doe <johndoe@email.com>
2022-11-28 17:14:53 +0100

    2

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -22,12 +22,13 @@ const getTotalPrice = (items = []) => {
 
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
-    const {tg} = useTelegram();
+    const {tg, queryId} = useTelegram();
 
     const onSendData = useCallback(() => {
         const data = {
             products: addedItems,
             totalPrice: getTotalPrice(addedItems),
+            queryId,
         }
         fetch('http://localhost:3000', {
             method: 'POST',
